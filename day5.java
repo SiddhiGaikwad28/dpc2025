cdb419b3b57d823d643bccffb4064f70dfb1a9f0
SiddhiGaikwad28 <siddhigaikwad2804@gmail.com>
2025-08-19 16:26:16 +0530

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class LeadersInArray {
+
+    public static ArrayList<Integer> findLeaders(int[] arr, int n) {
+        ArrayList<Integer> leaders = new ArrayList<>();
+
+
+        int maxFromRight = arr[n - 1];
+        leaders.add(maxFromRight);
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= maxFromRight) {
+                leaders.add(arr[i]);
+                maxFromRight = arr[i];
+            }
+        }
+
+        Collections.reverse(leaders);
+
+        return leaders;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        ArrayList<Integer> leaders = findLeaders(arr, n);
+
+
+        System.out.println("Leaders: " + leaders);
+
+        sc.close();
+    }
+}
